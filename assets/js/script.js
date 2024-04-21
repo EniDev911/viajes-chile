ac812b5a1451ae65fce7858b8f247b9983525c68
enidev911 <enidev911@gmail.com>
2024-04-20 23:17:22 -0400

    Mostrar el popup cuando se active el evento submit en el formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,17 @@ window.addEventListener("scroll", () => {
     ? nav.style.background = "#000"
     : nav.style.background = "transparent"
 })
+
+// form submit
+document.getElementById("formulario").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const date = new Date(Date.now());
+  const popup = document.getElementById('popup');
+  document.getElementById("date").textContent = date.toLocaleString();
+  const toast = bootstrap.Toast.getOrCreateInstance(popup);
+  toast.show()
+  setTimeout(() => {
+    toast.hide()
+  }, 2500)
+  e.target.reset();
+})
